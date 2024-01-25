7d4c5a58f2c85a4a303bffa716ee83e17ed53618
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2024-01-25 10:16:55 +0100

    login user

--- vue/src/views/login.vue
+++ vue/src/views/login.vue
@@ -7,29 +7,57 @@ const codigo = ref("");
 function entrar() {
   fetch(route("/login"), {
     method: "POST",
-    headers: {"Content-type": "plain/text"},
+    headers: { "Content-type": "plain/text" },
     body: codigo.value
   })
     .then((res) => {
-        return res.text()
+      return res.text()
     })
     .then((res) => {
-        if (res == "OK") {
-            console.log("logeado")    
-        }
+      if (res == "OK") {
+        console.log("logeado")
+      }
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     })
 }
 </script>
 
+
+
 <template>
-  <label for="codigo">Codigo:</label>
-  <input type="passwrod" id="codigo" v-model="codigo">
-  <button @click="entrar">Entrar</button>
-  
+  <div class="container-fluid d-flex flex-row bg-gradient-custom">
+    <form class="p-5">
+      <div class="mb-3 d-flex flex-column">
+        <label for="Codigo" class="form-label">Codigo</label>
+        <input type="password" class="form-control" id="Codigo">
+      </div>
+      <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
+.bg-gradient-custom {
+  background: linear-gradient(45deg, #F2A74B, #D98032);
+  height: 100vh;
+
+}
+
+.form-label {
+  color: black;
+}
+
+form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 20px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 0px 0px 60px #A65221;
+}
 </style>
