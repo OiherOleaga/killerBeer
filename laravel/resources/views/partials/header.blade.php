c144c5a4808aa3bca533525b7ba6da1c4bb23165
David Moreno Alonso <david.moreno@ikasle.egibide.org>
2024-01-30 10:13:20 +0100

    tablas

--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -32,6 +32,10 @@
                             class="block py-2 px-3 text-fourth  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-third md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Productos</a>
                     </li>
                     <li>
+                        <a href="/categorias"
+                            class="block py-2 px-3 text-fourth rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-third md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Categorias</a>
+                    </li>
+                    <li>
                         <a href="#"
                             class="block py-2 px-3 text-fourth rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-third md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Estadisticas</a>
                     </li>
