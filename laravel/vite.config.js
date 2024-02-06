574e3e839d545aa0ba372b31cea9cc6d91e3ab9d
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-02-06 13:52:06 +0100

    1alksdjfklasjkldjflka

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'css/app.css',
-                'js/app.js',
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
