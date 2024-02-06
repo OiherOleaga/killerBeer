22c81399b48906e297c91befe155fbc50c21ee20
David-Moreno-Alonso <david.moreno@ikasle.egibide.org>
2024-02-06 20:44:20 +0100

    1.1

--- laravel/resources/views/categorias/create.blade.php
+++ laravel/resources/views/categorias/create.blade.php
@@ -10,7 +10,7 @@
                     <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6 dark:bg-gray-800 dark:text-white">
                         <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3 dark:text-gray-900">
                             <div class="text-gray-600 lg:border-r-2">
-                                <p class="font-medium text-lg">Datos del formato</p>
+                                <p class="font-medium text-lg">Datos de la categoria</p>
                                 <p>Rellene todos los campos.</p>
                             </div>
                             <div class="lg:col-span-2">
