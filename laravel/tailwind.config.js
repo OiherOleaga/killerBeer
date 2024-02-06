f190e0d32a1b21c2c224384c8a93a54e12130e7d
David-Moreno-Alonso <david.moreno@ikasle.egibide.org>
2024-02-06 20:40:32 +0100

    tailwind config

--- laravel/tailwind.config.js
+++ laravel/tailwind.config.js
@@ -1,31 +1,39 @@
-// tailwind.config.js
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+],
     purge: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue'
     ],
-    darkMode: 'class', // Opcional: habilita el modo oscuro
+    darkMode: 'class',
     theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Figtree', 'sans-serif'],
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'lightdark': '#212226',
+                'first': '#F2CC85 ',
+                'second': '#F2A74B',
+                'third': '#D98032',
+                'fourth': '#A65221',
+            }
         },
-        colors: {
-          transparent: 'transparent',
-          current: 'currentColor',
-          white: '#ffffff',
-          lightdark: '#212226',
-          first: '#F2CC85',
-          second: '#F2A74B',
-          third: '#D98032',
-          fourth: '#A65221',
-        },
-      },
     },
+
     plugins: [
-      require('tailwindcss-plugins/pagination')
+        forms,
+
     ],
-  };
-  
+};
