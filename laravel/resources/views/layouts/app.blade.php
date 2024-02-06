a12074fdf124b3be75bdcac21e9849cc46316c9b
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-02-06 13:02:13 +0100

    fuera vite

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/css/app.css"> 
     <script>
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
                 '(prefers-color-scheme: dark)').matches)) {
